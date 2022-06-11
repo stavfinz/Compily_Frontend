463fda66085f764c33a005060bd594e29df63f96
גבריאל ז׳ורנו <riel@h-MacBook-Pro-sl-gbryl.local>
2022-06-11 22:06:55 +0300

    add addUI

--- compily-frontend/src/components/DoctorView/DoctorView.jsx
+++ compily-frontend/src/components/DoctorView/DoctorView.jsx
@@ -5,13 +5,18 @@ import './DoctorView.css';
 import Chip from '@mui/material/Chip';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
-import docData from '../../JSON/Doctors.json'
-import patiData from '../../JSON/Ronens_Allpatients.json'
+import docData from '../../JSON/Doctors.json';
+import patiData from '../../JSON/Ronens_Allpatients.json';
 
 const DoctorView = (props) => {
 
     const [patientFound, setPatientFound] = useState(false);
+    const [newTreatment, setNewTreatment] = useState(false);
+    const [treatName, setTreatName] = useState('');
+    const [treatDisc, setTreatDisc] = useState('');
+
 
     const handleDelete = () => {
         console.info('You clicked the delete icon.');
@@ -21,18 +26,29 @@ const DoctorView = (props) => {
     const handleClick = () => {
         console.log("Click!");
         setPatientFound(true);
-        //window.location.reload();
     }
 
     const logOut = () => {
         props.onLogOut();
     }
 
+    const openAddNewTreatment = () => {
+        setNewTreatment(true);
+    }
+
+    const addNewTreatment = () => {
+        setNewTreatment(false);
+    }
+
+
+
+
     return (
         <div className="patient-container">
             <h3>שלום {docData[0].firstName}</h3>
             <div className="patient-item">
                 <div className="search-container" >
+                    {patientFound ? <div className="search-item"><AddBoxIcon fontSize="large" color="primary" onClick={openAddNewTreatment} /></div> : null}
                     <div className="search-item">
                         <Button variant="contained" size='large' type="submit" onClick={handleClick}>חפש</Button>
                     </div>
@@ -45,6 +61,11 @@ const DoctorView = (props) => {
 
                 </div>
                 <br />
+                {newTreatment ? <div>
+                    <div className="diff-margin"><TextField fullWidth id="outlined-basic" label="שם טיפול" variant="outlined" onChange /></div>
+                    <div className="diff-margin"><TextField fullWidth id="outlined-basic" label="תיאור הטיפול" variant="outlined" /></div>
+                    <div className="diff-margin"><Button onClick={addNewTreatment}>הוסף</Button></div>
+                </div> : null}
                 {patientFound ? <PatientTable /> : null}
                 <br />
                 <div>
